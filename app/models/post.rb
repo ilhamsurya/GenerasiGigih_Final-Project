5289308123e2e61a16f77f2af977d75c7165ae85
ilhamsurya <ilhamsurya.m@gmail.com>
2021-08-16 13:44:45 +0700

    fix method get post with hashtag

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,10 +14,22 @@ class Post
         true
     end
 
+    def self.all_post(tag)
+        client = create_db_client
+        all_post = client.query("SELECT posts.user_id, posts.body, posts.attachment, users.username, hashtags.tag, post_hashtags.created_time 
+            FROM posts JOIN post_hashtags ON posts.id = post_hashtags.post_id 
+            JOIN hashtags ON hashtags.id = post_hashtags.hashtag_id 
+            JOIN users on posts.user_id = users.id WHERE hashtags.tag=\"#{db_client.escape(tag)}\" 
+            ORDER BY posts.created_time desc;").each
+        all_post
+    end
+
     def save
         return false unless valid?
         client = create_db_client
         save_post = client.query("INSERT INTO posts (user_id, body, attachment) values ('#{@user_id}', '#{@body}', \"#{@attachment}\")")
         save_post
     end
+
+
 end
