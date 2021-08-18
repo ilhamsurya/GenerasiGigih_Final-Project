6aeb35bd847f30c42bf869da912e39d369124741
ilhamsurya <ilhamsurya.m@gmail.com>
2021-08-18 12:39:16 +0700

    adding model save

---
+++ app/models/comment.rb
@@ -0,0 +1,17 @@
+class Comment
+    def initialize(user_id, post_id, body, attachment=nil)
+        @user_id = user_id
+        @body = body
+        @attachment = attachment
+        @post_id = post_id
+    end
+
+    def save
+        client = create_db_client
+        query = "INSERT INTO comments (user_id, post_id, body, attachment) VALUES ('#{@user_id}','#{@post_id}', '#{@body}', \"#{@attachment}\")"
+        client.query(query)
+        id = client.last_id
+        data = client.query("SELECT * FROM comments WHERE id = #{id}").each[0]
+        data
+    end
+end
