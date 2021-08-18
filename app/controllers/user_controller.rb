142a6d0c69039bf8719fba75ce99c71e08c1e20c
ilhamsurya <ilhamsurya.m@gmail.com>
2021-08-18 12:38:27 +0700

    refactoring user save

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,56 +8,36 @@ class UserController < Sinatra::Application
 
   def self.show_one(params)
     id = params['id']
-    user = User.get_by_id(id)
-    return [
-      200,
-      {
-          message: 'success loading a user',
-          user: user,
-      }.to_json
-    ]
+    user = User.get_by_id(id)    
   end
 
   def self.save(params) 
-    if data.nil?
-      return [
-        400,
-        {
-          message: "data not available, please try again",
-         
-        }.to_json
-      ]
-    elsif data['username'].nil? or data['username'].empty? or data['email'].nil? or data['email'].empty?
-      return [
-        400,
-        {
-          message:  "data not available, please make sure that the data already inputed",
-        }.to_json
-      ]
+    if params.nil?
+      return {
+        message: "data not available, please try again",
+      }.to_json
+
+    elsif params['username'].nil? || params['email'].nil?
+      return {
+        message:  "data not available, please make sure that the data already inputed",
+      }.to_json
+    
     end
 
     username = params['username']
     email = params ['email']
     description = params['description']
     user = User.new(username,email,description)
+
+    return  {
+      message: "succesfully created new user",
+      username: username,
+      email: email,
+      description: description
+    }.to_json
   end
 
-  def create_post(id,params)
-    client = create_db_client
-    user_id = id.to_i
-    body = params['body']
-    attachment = params['attachment']
- 
-    if attachment.nil?
-      post = Post.new(user_id, body, attachment)
-      data = post.save
-    else
-      post = Post.new(user_id, body, nil)
-      data = post.save
-    end
-    
-    username = client.query("SELECT username FROM users WHERE id = #{user_id}").each[0]['username']
-  end
+  
 
   
 end
