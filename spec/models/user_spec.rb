b62b3e8e6574e2969d38ceb3df7fcd86f7bbdd20
ilhamsurya <ilhamsurya.m@gmail.com>
2021-08-19 19:55:04 +0700

    fixing query user all

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,36 @@
 require_relative '../test_helper'
 require_relative '../../app/models/user'
+require_relative '../../app/controllers/user_controller'
+require_relative '../../db/db_connector'
 
 describe User do
-  let(:user_mock) {double}
+  describe "#validation" do
+    context "when initialize valid parameter" do
+      it "should return true" do
+          user = User.new("Gurame","Gurame@gmail.com")
+          expect(user.valid?).to eq(true)
+        end
+      end
+    context "when initialize invalid parameter" do
+      it "should return false" do
+          user = User.new("Gurame",nil)
+          expect(user.valid?).to eq(false)
+      end
+    end
+  end
 
-  describe ".initialize" do
+  describe "#initialize" do
     context "given valid attribute" do
-        it "should create object that equal with user_valid_attribute" do
+        it "should create object that equal with user_params" do
             user_params = {
                 "username" => "test",
                 "email" => "test@gmail.com"
             }
-            user = User.new(user_params["username"], user_params["email"])
-            expect(user.username).to  eq(user_params["username"])
-            expect(user.email).to  eq(user_params["email"])
+            username = user_params["username"]
+            email = user_params["email"]
+            user = User.new(username, email)
+            expect(user.username).to eq(username)
+            expect(user.email).to eq(email)
         end
     end
   end
@@ -21,10 +38,18 @@ describe User do
   describe '#get_all' do
     context 'when displaying all registered users' do
         it 'should return all registered users' do
-            get_all = "SELECT * FROM users"
-            raw_data = User.get_all
-            expect(user_mock).to receive(:query).with(get_by_id).and_return(raw_data)
-            expect(get_by_id).to eq(raw_data)
+            mock_client = double
+            select_query = "SELECT * FROM users"
+            mock_raw_data = [
+                {
+                    "username" => "ilham",
+                    "email" => "ilhamsurya.m@gmail.com",
+                }
+            ]
+            
+            allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+            expect(mock_client).to receive(:query).with(select_query).and_return(mock_raw_data)
+            User.get_all
         end
     end
   end
@@ -32,30 +57,50 @@ describe User do
   describe '#get_one' do
     context 'when displaying single registered users' do
         it 'should return single registered users' do
-            user_params = {
-                "id" => 1
-            }
-            get_by_id = "SELECT * FROM users WHERE id = #{user_params["id"]}"
-            raw_data = [user_params]                
+            mock_client = double
+            id = 1
+            select_query_with_id = "SELECT * FROM users WHERE id = #{id}"
+            mock_raw_data = [
+                {
+                    "username" => "ilham",
+                    "email" => "ilhamsurya.m@gmail.com",
+                }
+            ]
 
-            expect(user_mock).to receive(:query).with(get_by_id).and_return(raw_data)
-            expect(get_by_id).to include(id) 
+            allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+            expect(mock_client).to receive(:query).with(select_query_with_id).and_return(mock_raw_data)
 
-            user = User.get_by_id(id)
-
-            expect(user.id).to eq(id) 
+            User.get_by_id(id)
         end
     end
   end
 
   describe "#save" do
       context "with valid params" do
-        it "should save user data"
+        it "should save user data" do
+          mock_client = double
           
-
+          user_params = {
+              "username" => "test",
+              "email" => "test@ymail"
+          }
+          insert_query = "INSERT INTO users (username, email) VALUES ('#{user_params["username"]}', '#{user_params["email"]}')"
+          allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+          expect(mock_client).to receive(:query).with(insert_query)
+          user = User.new(user_params["username"], user_params["email"])
+          user.save
+        end
       end
       context "with invalid params" do
-        it "should return warning"
+        it "shouldnt save any data" do
+          user_params = {
+              "username" => nil,
+              "email" => "test@ymail"
+          }
+          user = User.new(user_params["username"], user_params["email"])
+          valid_user = user.valid?
+          expect(valid_user).to eq(false)
       end
+    end
   end
 end
