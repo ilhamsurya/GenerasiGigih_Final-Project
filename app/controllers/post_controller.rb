0a84dc39083cb1faafacefd3573d3cfadf897e33
ilhamsurya <ilhamsurya.m@gmail.com>
2021-08-20 09:41:22 +0700

    adding posting a comment without attachment

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -12,39 +12,48 @@ class PostController < Sinatra::Application
       message: "successfuly load post with tag #{tag}",
       posts: post
     }.to_json
-    
   end
 
   def self.show_trending
     tags = Hashtag.trending_tags
     return {
       message: "successfuly load 5 trending hashtags in 24 hour",
-      posts: tags
+      trending: tags
     }.to_json
   end
   
   def self.create_post(params,data)
       client = create_db_client
-      user_id = params['id']
+      user_id = params.to_i
       body = data['body']
       attachment = data["attachment"]
       hashtags = data['hashtags']
       username = User.get_username(user_id)
-      data_attachment = adding_post_attachment(user_id,body,attachment)
-      data_tags = adding_tags(hashtags)
-
-      data_tags.each do |tag|
-        insert_post_tags = "INSERT INTO post_hashtags(post_id,hashtag_id) VALUES (#{data_attachment['id']}, #{tag['id']})"
-        rawData = client.query(insert_post_tags)
+      data_post = adding_post_attachment(user_id,body,attachment)
+      
+      if hashtags.nil?
+        return  {
+          message: "succesfully creating new post",
+          user_id: user_id,
+          post_id: data_post['id'],
+          user_info: username,
+          body: body,
+        }.to_json
+      else
+        data_tags = adding_tags(hashtags)
+        data_tags.each do |tag|
+          insert_post_tags = "INSERT INTO post_hashtags(post_id,hashtag_id) VALUES (#{data_post['id']}, #{tag['id']})"
+          rawData = client.query(insert_post_tags)
+        end
       end
 
-      return{
+      return  {
         message: "succesfully creating new post",
         user_id: user_id,
-        post_id: data_attachment['id'],
+        post_id: data_post['id'],
         user_info: username,
         body: body,
-        attachment: data_attachment['attachment'],
+        attachment: data_post['attachment'],
         tags: data_tags
       }.to_json
 
@@ -52,21 +61,32 @@ class PostController < Sinatra::Application
 
   def self.create_comment(params,data)
       client = create_db_client
-      user_id = params['id']
-      post_id = params['post_id']
+      user_id = params['id'].to_i
+      post_id = params['post_id'].to_i
       body = data['body']
       attachment = data["attachment"]
       hashtags = data['hashtags']
       username = User.get_username(user_id)
       data_comment = adding_comment_attachment(user_id,post_id,body,attachment)
-      data_tags = adding_tags(hashtags)
 
-      data_tags.each do |tag|
-        insert_post_tags = "INSERT INTO comment_hashtags(comment_id,hashtag_id) VALUES (#{data_comment['id']}, #{tag['id']})"
-        rawData = client.query(insert_post_tags)
+      if hashtags.nil?
+        return  {
+          message: "succesfully creating new comment to the post #{data_comment['id']}",
+          user_id: user_id,
+          post_id: post_id,
+          comment_id: data_comment['id'],
+          user_info: username,
+          body: body,
+        }.to_json
+      else
+        data_tags = adding_tags(hashtags)
+        data_tags.each do |tag|
+          insert_post_tags = "INSERT INTO comment_hashtags(comment_id,hashtag_id) VALUES (#{data_comment['id']}, #{tag['id']})"
+          rawData = client.query(insert_post_tags)
+        end
       end
 
-      return{
+      return {
         message: "succesfully creating new comment to the post #{data_comment['id']}",
         user_id: user_id,
         post_id: post_id,
@@ -76,9 +96,29 @@ class PostController < Sinatra::Application
         attachment: data_comment['attachment'],
         tags: data_tags
       }.to_json
-
   end
 
+  def self.validating_attachment(file_name,tempfile)
+    accepted_formats = ['.jpg', '.JPG','.png','.PNG','.gif','.mp4','.mkv']
+    is_accepted_format = accepted_formats.include? File.extname(file_name)
+
+    if is_accepted_format
+      File.open("./public/#{file_name}", 'wb') do |f|
+        f.write(tempfile.read)
+      end
+      file_path = "http://localhost:4567/#{file_name}"
+      file_path
+    else
+      return [400, { message: 'file you want to send is not accepted format' }]
+    end
+  end
+
+  def self.adding_tags(tags)
+    post_tags = Hashtag.new(tags)
+    data_tags = post_tags.save
+    data_tags
+  end
+  
   def self.adding_post_attachment(user_id,body,attachment)
     if attachment.nil?
       post = Post.new(user_id, body)
@@ -99,9 +139,6 @@ class PostController < Sinatra::Application
     data_comment
   end
 
-  def self.adding_tags(tags)
-    post_tags = Hashtag.new(tags)
-    data_tags = post_tags.save
-    data_tags
-  end
+
+  
 end
