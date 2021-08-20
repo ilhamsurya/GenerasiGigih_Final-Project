3c1457e8acedefc4bf43f3f505115be514686841
ilhamsurya <ilhamsurya.m@gmail.com>
2021-08-20 09:38:27 +0700

    fixing comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment
+    attr_accessor :user_id, :post_id, :body, :attachment
     def initialize(user_id, post_id, body, attachment=nil)
         @user_id = user_id
         @body = body
@@ -6,6 +7,9 @@ class Comment
         @post_id = post_id
     end
 
+    def valid?
+        return false if @user_id.nil? || @post_id.nil? || @body.nil?
+    end
     def save
         client = create_db_client
         query = "INSERT INTO comments (user_id, post_id, body, attachment) VALUES ('#{@user_id}','#{@post_id}', '#{@body}', \"#{@attachment}\")"
