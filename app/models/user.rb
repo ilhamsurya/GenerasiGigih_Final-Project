44eae43a8beb02f8c08a9d1a9943f344fadefb10
ilhamsurya <ilhamsurya.m@gmail.com>
2021-08-20 09:39:35 +0700

    refactoring user get username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 require_relative '../../db/db_connector'
 class User
-    attr_accessor :username, :email, :description
-    def initialize(username, email, description)
+    attr_accessor :username, :email
+    def initialize(username, email)
         @username = username
         @email = email
-        @description = description
     end
 
     def valid?
@@ -14,45 +13,45 @@ class User
 
     def save
         client = create_db_client
-        insert_query = "INSERT INTO users (username, email, description) VALUES ('#{@username}', '#{@email}', '#{@description}')"
+        insert_query = "INSERT INTO users (username, email) VALUES ('#{@username}', '#{@email}')"
         client.query(insert_query)
     end
 
     def self.get_all
         client = create_db_client
-        @user = Hash.new
-        @users = Array.new
-        @query = client.query('SELECT * FROM users')
-        @query.each do |data|
-            @user = {:id => data['id'], :name => data['username'], :email => data['email'], 
-                :desc => data['description'], :created_at => data['created_at']}
-            @users << @user
+        user = Hash.new
+        users = Array.new
+        query = client.query('SELECT * FROM users')
+        query.each do |data|
+            user = {:id => data['id'], :name => data['username'], :email => data['email'], 
+                :created_at => data['created_at']}
+            users << user
         end
-        return @users.to_json
+        users
     end
 
     def self.get_by_id(id)
         client = create_db_client
-        @user = Hash.new
-        @users = Array.new
-        @query = client.query("SELECT * FROM users WHERE id = #{id}")
-        @query.each do |data|
-            @user = {:id => data['id'], :name => data['username'], :email => data['email'], 
-                :desc => data['description'], :created_at => data['created_at']}
-            @users << @user
+        user = Hash.new
+        users = Array.new
+        query = client.query("SELECT * FROM users WHERE id = #{id}")
+        query.each do |data|
+            user = {:id => data['id'], :name => data['username'], :email => data['email'], 
+                :created_at => data['created_at']}
+            users << user
         end
-        return @users.to_json
+        users
     end
 
     def self.get_username(id)
         client = create_db_client
-        @user = Hash.new
-        @users = Array.new
-        @query = client.query("SELECT username,email FROM users WHERE id = #{id}")
-        @query.each do |data|
-            @user = {:name => data['username'],:email => data['email']}
-            @users << @user
+        user = Hash.new
+        users = Array.new
+        query = client.query("SELECT username,email FROM users WHERE id = #{id}")
+        query.each do |data|
+            user = {:name => data['username'],:email => data['email']}
+            users << user
         end
-        return @users
+        users
     end
 end
