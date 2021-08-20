945b6dc18f57bd2ee2834431cb2b2aaf42880d16
ilhamsurya <ilhamsurya.m@gmail.com>
2021-08-20 09:45:35 +0700

    adding testing for saving user

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -8,31 +8,56 @@ describe UserController do
         context "see all registered user" do
             it "should call method show_all" do            
                 users = User.get_all
-                actual_user = UserController.show_all
-                expect(users).to eq(actual_user)
+                query_user = UserController.show_all
+                actual_user = {
+                    message: "returning all user",
+                    users: users
+                }.to_json
+                expect(actual_user).to eq(query_user)
             end
         end
     end
 
     describe "#show_by_id" do
         context "see single registered user by id" do
-            it "should call method show_one" do            
-                user_params = {
+            it "should call method show_one" do
+                params = {
                     "id" => 1
-                }
-                users = User.get_one(user_params["id"])
-                actual_user = UserController.show_one(user_params["id"])
-                expect(users).to eq(actual_user)
+                }            
+                users = User.get_by_id(params["id"])
+                query_user = UserController.show_one(params)
+                actual_user = {
+                    message: "returning single user",
+                    users: users
+                }.to_json
+                expect(actual_user).to eq(query_user)
             end
         end
     end
-    describe ".save" do
-        context "given user_attribute" do
-            it "should save user along with the attribute defined" do
-                user_params = {
-                    "username" => 1
-                    ""
+    describe "#show_by_username" do
+        context "see single username registered user by id" do
+            it "should call method get_username" do            
+                params = {
+                    "id" => 1
                 }
+                users = User.get_username(params["id"])
+                actual_user = UserController.show_one(params)
+                expect(users["username"]).to eq(actual_user["username"])
+            end
+        end
+    end
+    describe "#save" do
+        context "given valid user_params" do
+            it "should save user along with the attribute defined" do
+                user_mock = double
+                user_params = {
+                    "username" => "Hal Jordan",
+                    "email" => "haljordan@ymail.co.id"
+                }
+                expect(User).to receive(:new).with(user_params["username"], user_params["email"]).and_return(user_mock)
+                allow(user_mock).to receive(:save) 
+
+                UserController.save(user_params)
             end
         end
     end
